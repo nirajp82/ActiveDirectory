20de2c83bd9ebacbe49fb6f9b161e60ff51261f7
nirajp82 <61636643+nirajp82@users.noreply.github.com>
2023-05-18 10:59:38 -0400

    Update CreateADGroupAndUser.ps1

--- CreateADGroupAndUser.ps1
+++ CreateADGroupAndUser.ps1
@@ -9,10 +9,18 @@ foreach ($groupName in $groupNames) {
     New-ADGroup -Name $groupName -GroupCategory Security -GroupScope Global
 }
 
+ $password = "testTEST1234"
 # Add users to the groups
 foreach ($groupName in $groupNames) {
     $group = Get-ADGroup -Filter { Name -eq $groupName }
     foreach ($userName in $userNames) {
+    $firstName = $userName 
+    $lastName = "" 
+     # Combine the first name and last name to create the user's full name
+    $fullName = "$firstName $lastName"
+    
+           # Create the user with the given full name and password
+    New-ADUser -Name $fullName -GivenName $firstName -Surname $lastName -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) -Enabled $true
         $user = Get-ADUser -Filter { Name -eq $userName }
         Add-ADGroupMember -Identity $group -Members $user
     }
