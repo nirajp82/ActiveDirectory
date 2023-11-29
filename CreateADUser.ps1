963e78bd275f1d664c5e9be3920dfbb29b22eac2
nirajp82 <61636643+nirajp82@users.noreply.github.com>
2023-11-29 10:14:28 -0500

    Update CreateADUser.ps1

--- CreateADUser.ps1
+++ CreateADUser.ps1
@@ -24,20 +24,77 @@ Import-Module ActiveDirectory
 # Set the common password for all users
 $password = ConvertTo-SecureString -String "PasswordGoesHere" -AsPlainText -Force
 
-# Specify the Organizational Unit (OU) path
-$ouPath = "OU=Users,DC=yourdomain,DC=com"
-
+# Specify the domain controller path
+$dcPath = "DC=yourdomain,DC=com"
 # Specify the UPN suffix
 $upnSuffix = "yourdomain.com"
-
-# Create 5 users
-for ($i = 1; $i -le 5; $i++) {
-    $givenName = "User$i"
-    $surName = "LastName$i"
-    $displayName = "User LastName $i"
-    $samAccountName = "user$i"
-    $emailAddress = "user$i@$upnSuffix"
-    $userPrincipalName = "user$i@$upnSuffix"
-
-    New-ADUser -Name $displayName -GivenName $givenName -Surname $surName -DisplayName $displayName -SamAccountName $samAccountName -EmailAddress $emailAddress -UserPrincipalName $userPrincipalName -AccountPassword $password -Path $ouPath -Enabled $true
+#Name of the OU
+$ouName = "MyPSOU"
+# Specify the Organizational Unit (OU) path
+$ouPath = "OU=$ouName,$dcPath"
+$maxUserCnt = 50
+# Check if the OU exists, create it if not
+if (-not (Get-ADOrganizationalUnit -Filter {DistinguishedName -eq $ouPath})) {
+    New-ADOrganizationalUnit -Name $ouName -Path $dcPath
 }
+
+$groups = @("Group1", "Group2", "Group3", "Group4", "Group5", "GroupAll")# Array of group names
+
+Write-Host "Create users. Count: $maxUserCnt"
+# Create users
+for ($cnt = 1; $cnt -le $maxUserCnt; $cnt++) {
+    $givenName = "UserPS$cnt"
+    $surName = "LastName$cnt"
+    $displayName = "UserPS LastName$cnt"
+    $samAccountName = "userPS$cnt"
+    $emailAddress = "userPS$cnt@$upnSuffix"
+    $userPrincipalName = "userPS$cnt@$upnSuffix"
+	
+	# Check if the user exists, remove it if it does
+    $existingUser = Get-ADUser -Filter {SamAccountName -eq $samAccountName}
+    if ($existingUser) {
+        Remove-ADUser -Identity $existingUser -Confirm:$false
+    }
+
+    $newUser = New-ADUser -Name $displayName -GivenName $givenName -Surname $surName -DisplayName $displayName -SamAccountName $samAccountName -EmailAddress $emailAddress -UserPrincipalName $userPrincipalName -AccountPassword $password -Enabled $true -Path $ouPath -PassThru
+	
+	$mgrId = 1
+	if($cnt -ge 10)
+	{
+		$mgrId = $cnt - 10
+	}	
+	$managerSamAccountName =  "userPS$mgrId";
+	
+	# Get the manager's DistinguishedName
+	$managerDN = Get-ADUser -Filter {SamAccountName -eq $managerSamAccountName} | Select-Object -ExpandProperty DistinguishedName
+
+	# Set the manager for the new user
+	Set-ADUser -Identity $samAccountName -Manager $managerDN	
+}
+
+Write-Host "Users are created, Adding users to the group."
+
+$cnt = 1
+foreach ($groupToAddUser in $groups) {
+	# Check if the group exists, create it if not
+	if (-not (Get-ADGroup -Filter {Name -eq $groupToAddUser})) {
+		Write-Host "Creating group: $groupToAddUser."
+		New-ADGroup -Name $groupToAddUser -GroupScope Global -Path $ouPath -Description "Dummy PS group - $groupToAddUser"
+	}
+	
+	if($groupToAddUser -eq "GroupAll") {
+		$startIndex = 0
+		$pageSize = $maxUserCnt+1
+	}
+	else {
+		$pageSize = $maxUserCnt/$groups.Count
+		$startIndex = ($cnt - 1) * $pageSize
+	}
+
+	$users = Get-ADUser -Filter {Enabled -eq $true} -SearchBase $ouPath | Select-Object -Skip $startIndex -First $pageSize
+
+	Add-ADGroupMember -Identity $groupToAddUser -Members $users
+	$cnt++
+}
+
+Write-Host "Done."
